255ad33dab780e5cf7e63c95b1014aeba10966d4
dillonzellis <dillonzellis@gmail.com>
2023-08-28 11:00:27 -0400

    remove pb from notification-bar

--- includes/notification-bar.php
+++ includes/notification-bar.php
@@ -1,5 +1,5 @@
 <div style="color:#1D1D1D;background-color: #E2F000;">
     <div class="notice-banner">
-        <div style="padding-bottom: 1rem;">Our branches will be closed on Monday, September 4, 2023 in observance of Labor Day.</div>
+        <div>Our branches will be closed on Monday, September 4, 2023 in observance of Labor Day.</div>
     </div>
 </div>
