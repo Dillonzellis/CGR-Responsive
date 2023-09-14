150790822bea27e5736ba858113ccfee201d4db2
dillonzellis <dillonzellis@gmail.com>
2023-09-14 12:32:20 -0400

    format main and index

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
-<html lang="en"><head><title>WEBSITE REDIRECT</title>
-<meta http-equiv="refresh" content="0; URL=https://www.cgrcu.org/main.php">
+<html lang="en">
 
-<meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder or an owner of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
-<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1"></head>
+<head>
+  <title>WEBSITE REDIRECT</title>
+  <meta http-equiv="refresh" content="0; URL=https://www.cgrcu.org/main.php">
+
+  <meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder or an owner of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
+  <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
+</head>
 
 <body>
 
 
 
 
-</body></html>
+</body>
+
+</html>
