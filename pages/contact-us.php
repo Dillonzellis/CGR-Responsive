de375526476a7f5bd49df0c27ab7c9e7c74fc3ec
dillonzellis <dillonzellis@gmail.com>
2023-09-14 12:32:28 -0400

    update contact info

--- pages/contact-us.php
+++ pages/contact-us.php
@@ -2,176 +2,195 @@
 <html lang="en">
 
 <head>
-<title>CGR Credit Union - Contact Us</title>
+  <title>CGR Credit Union - Contact Us</title>
 
-<!--META DATA-->
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1"/>
-<meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder – or an owner – of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
-<meta name="keywords" content=""/>
+  <!--META DATA-->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="Like all credit unions, CGR Credit Union is a not-for-profit financial cooperative. When you join the credit union, you become a shareholder – or an owner – of a very unique financial institution. Earnings above the required reserves are returned to you, in the form of lower interest rates on loans and competitive dividends on savings.">
+  <meta name="keywords" content="" />
 
-<?php include ('../includes/head.php'); ?>
-	<style>
-.branch-table{width:100%;}
-.branch-table td{width:33%; vertical-align:top; padding:5px;}
-</style>
+  <?php include('../includes/head.php'); ?>
+  <style>
+    .branch-table {
+      width: 100%;
+    }
+
+    .branch-table td {
+      width: 33%;
+      vertical-align: top;
+      padding: 5px;
+    }
+  </style>
 
 </head>
 
 <body>
-<?php include_once("../includes/analyticstracking.php") ?>
-<?php include ('../includes/header.php');?>
+  <?php include_once("../includes/analyticstracking.php") ?>
+  <?php include('../includes/header.php'); ?>
 
-<main id="main" class="content page">
+  <main id="main" class="content page">
 
-<div class="container">
-<div class="nine columns">
-<h1 class="page-title">Contact Us</h1>
-
-
- 	
-<div class="row p15-b">
-
-	<p><a href="mailto:msoq@cgrcu.org">Click here to contact a Member Services via email</a> for requests other than for loans or membership. For your protection, please do not send sensitive information such as PIN, account, and/or social security numbers.  Tell us the nature of your request and provide contact information.  We will contact you within two hours of your email (not a live chat) if sent during normal business hours or before 10:00 a.m. the next business day if sent before or after hours for our <strong>Ingleside Branch</strong>. </p>
-</div><!--end row-->
-
-<p class="df-title gbd_red fw600">Locations</p>
-
-<div class="clearfix"></div>
+    <div class="container">
+      <div class="nine columns">
+        <h1 class="page-title">Contact Us</h1>
 
 
 
-<table class="branch-table" role="presentation">
-<tr>
-<td>
-<p class="contact-page-subtitle">Main Office</p>
-<p class="contact-page-blurb"><a href="https://www.google.com/maps/place/2040+Ingleside+Ave,+Macon,+GA+31204/@32.8544514,-83.6491755,17z/data=!3m1!4b1!4m5!3m4!1s0x88f3feedbf970d09:0xf6dc56e3a12a787a!8m2!3d32.8544514!4d-83.6469868">2040 Ingleside Ave.<br/>Macon, GA 31204</a><br/>
-<strong>Phone:</strong> 478.745.0494<br/>
-<strong>Toll-Free:</strong> 800.633.2275<br/>
-<strong>Fax:</strong> 478.738.8582<br/>
-</p>
-<p><strong>Lobby & Drive-Thru Hours:</strong><br/>
-<strong>Monday - Friday</strong> <br/>8:30 a.m. to 5:00 p.m.<br/>
-<strong>Fridays</strong> <br/>Drive-Thru open until 6:00 p.m.<br/>
-<strong>Saturday</strong> <br/>8:30 a.m. to 12:00 Noon
-</p>
-</td>
+        <div class="row p15-b">
 
-<td>
-<p class="contact-page-subtitle">Navicent Health Branch</p>
-<p class="contact-page-blurb"><a href="https://www.google.com/maps/place/770+Pine+St,+Macon,+GA+31201/@32.8339639,-83.6376548,17z/data=!3m1!4b1!4m5!3m4!1s0x88f3fe8745dfcec3:0xf4be53dc14bab470!8m2!3d32.8339639!4d-83.6354661">770 Pine St. Suite L-20<br/>Macon, GA 31201</a><br/>
-<strong>Phone:</strong> 478.745.0494<br/>
-<strong>Toll-Free:</strong> 800.633.2275<br/>
-<strong>Fax:</strong> 478.738.8589</p>
+          <p><a href="mailto:msoq@cgrcu.org">Click here to contact a Member Services via email</a> for requests other than for loans or membership. For your protection, please do not send sensitive information such as PIN, account, and/or social security numbers. Tell us the nature of your request and provide contact information. We will contact you within two hours of your email (not a live chat) if sent during normal business hours or before 10:00 a.m. the next business day if sent before or after hours for our <strong>Ingleside Branch</strong>. </p>
+        </div><!--end row-->
 
-<p><strong>Lobby Hours:</strong><br/>
-<strong>Monday - Friday</strong> <br/>
-8:00 a.m. to 4:00 p.m. 
-</p></td>
+        <p class="df-title gbd_red fw600">Locations</p>
 
-<td>
-<p class="contact-page-subtitle">Baldwin Branch</p>
-<p class="contact-page-blurb"><a href="https://www.google.com/maps/place/571+Hammock+Rd+NW+%23100,+Milledgeville,+GA+31061/@33.0992857,-83.2532813,17z/data=!3m1!4b1!4m5!3m4!1s0x88f6b87550ff2029:0x12464fe437b76372!8m2!3d33.0992857!4d-83.2510926">Columbia Professional Center<br/>571 Hammock Road, Suite 100<br/>
-Milledgeville, GA 31061</a><br/>
-<strong>Phone:</strong> 478.745.0494<br/>
-<strong>Toll-Free:</strong> 800.633.2275<br/>
-<strong>Fax:</strong> 478.454.5437</p>
-<p><strong>Lobby Hours:</strong><br/>
-<strong>Monday - Friday</strong> <br/>
-8:30 a.m. to 5:00 p.m.
-</p>
-</td>
-</tr>
+        <div class="clearfix"></div>
 
 
-<tr>
-<td><p class="contact-page-subtitle">Warner Robins Branch<br>(Closing Sept. 8, 2023)</p>
-  <p class="contact-page-blurb"><a href="https://www.google.com/maps/place/104+N+Houston+Rd,+Warner+Robins,+GA+31093/@32.620351,-83.6433957,17z/data=!3m1!4b1!4m5!3m4!1s0x88f3e10c17260cfb:0xd8593390cefc7f0e!8m2!3d32.620351!4d-83.641207">104 N Houston Road<br/>
-    Warner Robins, GA 31093</a><br/>
-    <strong>Phone:</strong> 478.745.0494<br/>
-    <strong>Toll-Free:</strong> 800.633.2275<br/>
-    <strong>Fax:</strong> 478.922.7450</p>
-  <p><strong>Lobby & Drive-Thru Hours:</strong><br/>
-    <strong>Monday - Friday</strong> <br/>
-    8:30 a.m. to 5:00 p.m. <br/>
-    <strong>Saturday</strong> <br/>
-    8:30 a.m. to 12:00 Noon </p>
-  <p class="contact-page-subtitle"><!--<strong>Monday, Wednesday</strong> <br/>
+
+        <table class="branch-table" role="presentation">
+          <tr>
+            <td>
+              <p class="contact-page-subtitle">Main Office</p>
+              <p class="contact-page-blurb"><a href="https://www.google.com/maps/place/2040+Ingleside+Ave,+Macon,+GA+31204/@32.8544514,-83.6491755,17z/data=!3m1!4b1!4m5!3m4!1s0x88f3feedbf970d09:0xf6dc56e3a12a787a!8m2!3d32.8544514!4d-83.6469868">2040 Ingleside Ave.<br />Macon, GA 31204</a><br />
+                <strong>Phone:</strong> 478.745.0494<br />
+                <strong>Toll-Free:</strong> 800.633.2275<br />
+                <strong>Fax:</strong> 478.738.8582<br />
+              </p>
+              <p><strong>Lobby & Drive-Thru Hours:</strong><br />
+                <strong>Monday - Friday</strong> <br />8:30 a.m. to 5:00 p.m.<br />
+                <strong>Fridays</strong> <br />Drive-Thru open until 6:00 p.m.<br />
+                <strong>Saturday</strong> <br />8:30 a.m. to 12:00 Noon
+              </p>
+            </td>
+
+            <td>
+              <p class="contact-page-subtitle">Navicent Health Branch</p>
+              <p class="contact-page-blurb"><a href="https://www.google.com/maps/place/770+Pine+St,+Macon,+GA+31201/@32.8339639,-83.6376548,17z/data=!3m1!4b1!4m5!3m4!1s0x88f3fe8745dfcec3:0xf4be53dc14bab470!8m2!3d32.8339639!4d-83.6354661">770 Pine St. Suite L-20<br />Macon, GA 31201</a><br />
+                <strong>Phone:</strong> 478.745.0494<br />
+                <strong>Toll-Free:</strong> 800.633.2275<br />
+                <strong>Fax:</strong> 478.738.8589
+              </p>
+
+              <p><strong>Lobby Hours:</strong><br />
+                <strong>Monday - Friday</strong> <br />
+                8:00 a.m. to 4:00 p.m.
+              </p>
+            </td>
+
+            <td>
+              <p class="contact-page-subtitle">Baldwin Branch</p>
+              <p class="contact-page-blurb"><a href="https://www.google.com/maps/place/571+Hammock+Rd+NW+%23100,+Milledgeville,+GA+31061/@33.0992857,-83.2532813,17z/data=!3m1!4b1!4m5!3m4!1s0x88f6b87550ff2029:0x12464fe437b76372!8m2!3d33.0992857!4d-83.2510926">Columbia Professional Center<br />571 Hammock Road, Suite 100<br />
+                  Milledgeville, GA 31061</a><br />
+                <strong>Phone:</strong> 478.745.0494<br />
+                <strong>Toll-Free:</strong> 800.633.2275<br />
+                <strong>Fax:</strong> 478.454.5437
+              </p>
+              <p><strong>Lobby Hours:</strong><br />
+                <strong>Monday - Friday</strong> <br />
+                8:30 a.m. to 5:00 p.m.
+              </p>
+            </td>
+          </tr>
+
+
+          <tr>
+            <td>
+              <p class="contact-page-subtitle">Lake Joy Branch<br>(Opening Sept. 12, 2023)</p>
+              <p class="contact-page-blurb"><a href="https://www.google.com/maps/place/1110+SR+96,+Kathleen,+GA+31047/@32.5485448,-83.6945618,17z/data=!3m1!4b1!4m6!3m5!1s0x88f3ddee21ecdbbd:0x800b97ad8a34b0a2!8m2!3d32.5485403!4d-83.6919869!16s%2Fg%2F11b8v4bc9k?entry=ttu">1110 Highway 96<br>Kathleen, GA 31047</a><br />
+                <strong>Phone:</strong> 478.745.0494<br />
+                <strong>Toll-Free:</strong> 800.633.2275<br />
+                <strong>Fax:</strong> 478.922.7450
+              </p>
+              <p><strong>Lobby & Drive-Thru Hours:</strong><br />
+                <strong>Monday - Friday</strong> <br />
+                8:30 a.m. to 5:00 p.m. <br />
+                <strong>Saturday (Drive-Thru Only)</strong> <br />
+                8:30 a.m. to 12:00 Noon
+              </p>
+              <p class="contact-page-subtitle"><!--<strong>Monday, Wednesday</strong> <br/>
 8:00 a.m. to 4:00 p.m.<br>
 <strong>Tuesday, Thursday, Friday CLOSED</strong>--></p>
-</td>
 
+            </td>
 
-	<td><p class="contact-page-subtitle">Lake Joy Branch<br>(Opening Sept. 12, 2023)</p>
-  <p class="contact-page-blurb"><a href="https://www.google.com/maps/place/1110+SR+96,+Kathleen,+GA+31047/@32.5485448,-83.6945618,17z/data=!3m1!4b1!4m6!3m5!1s0x88f3ddee21ecdbbd:0x800b97ad8a34b0a2!8m2!3d32.5485403!4d-83.6919869!16s%2Fg%2F11b8v4bc9k?entry=ttu">1110 Highway 96<br>Kathleen, GA 31047</a><br/>
-    <strong>Phone:</strong> 478.745.0494<br/>
-    <strong>Toll-Free:</strong> 800.633.2275<br/>
-    <strong>Fax:</strong> 478.922.7450</p>
-  <p><strong>Lobby & Drive-Thru Hours:</strong><br/>
-    <strong>Monday - Friday</strong> <br/>
-    8:30 a.m. to 5:00 p.m. <br/>
-    <strong>Saturday (Drive-Thru Only)</strong> <br/>
-    8:30 a.m. to 12:00 Noon </p>
-  <p class="contact-page-subtitle"><!--<strong>Monday, Wednesday</strong> <br/>
+            <td>
+              <p class="contact-page-subtitle">Warner Robins Branch<br><strong>Now Closed</strong></p>
+              <p class="contact-page-blurb"><a href="https://www.google.com/maps/place/104+N+Houston+Rd,+Warner+Robins,+GA+31093/@32.620351,-83.6433957,17z/data=!3m1!4b1!4m5!3m4!1s0x88f3e10c17260cfb:0xd8593390cefc7f0e!8m2!3d32.620351!4d-83.641207">104 N Houston Road<br />
+                  Warner Robins, GA 31093</a><br />
+              </p>
+              <p><strong>Lobby & Drive-Thru Hours:</strong><br />
+                <strong>Monday - Friday</strong> <br />
+                8:30 a.m. to 5:00 p.m. <br />
+                <strong>Saturday</strong> <br />
+                8:30 a.m. to 12:00 Noon
+              </p>
+              <p class="contact-page-subtitle"><!--<strong>Monday, Wednesday</strong> <br/>
 8:00 a.m. to 4:00 p.m.<br>
 <strong>Tuesday, Thursday, Friday CLOSED</strong>--></p>
-  
-</td>
-<td>
-	<p><a href="https://www.cgrcu.org/pages/holiday-hours.php">Click here to view our Holiday hours</a>.</p>
-    <p><a href="https://www.cgrcu.org/pages/locations.php">Click here to view our ATM Locations.</a></td>
-</tr>
-</table>
+            </td>
 
 
-<div class="clearfix m30-b"></div>
-
-<p class="df-title gbd_red fw600">Additional Information</p>
-
-<div class="clearfix"></div>
-
-<table class="branch-table" role="presentation">
-<tr>
-<td><p class="df-mtitle fw600 gbd_red">Visa<sup>&reg;</sup> Credit Cards</p>
-<p>
-
-<strong>Customer Service:</strong> 800.423.7503<br/>
-
-<strong>Lost/Stolen:</strong> 800.325.3678<br/>
-
-<strong>Payment Address:</strong><br/>
-2040 Ingleside Avenue<br/>
-Macon, GA 31204<br/>
-	
-	For help with ezcardinfo call 866.604.0380</p></td>
-	<td><p class="df-mtitle fw600 gbd_red">Visa<sup>&reg;</sup> Debit/ATM Cards</p>
-<p><strong>Lost/Stolen:</strong> 800.472.3272</p>
-
-<p class="df-mtitle fw600 gbd_red">Timely Teller</p>
-<p><strong>24 Hour Phone Number:</strong> 478.744.0670</p>
-</td>
-</tr>
-
-</table>
+            <td>
+              <p><a href="https://www.cgrcu.org/pages/holiday-hours.php">Click here to view our Holiday hours</a>.</p>
+              <p><a href="https://www.cgrcu.org/pages/locations.php">Click here to view our ATM Locations.</a>
+            </td>
+          </tr>
+        </table>
 
 
-<div class="clearfix"></div>
+        <div class="clearfix m30-b"></div>
 
-</div><!--end nine columns-->
+        <p class="df-title gbd_red fw600">Additional Information</p>
 
-<div class="three columns sidebar">
-<?php include ('../includes/about-us-sidebar.php'); ?>
-</div><!--end three columns sidebar-->
+        <div class="clearfix"></div>
 
-</div><!--end container-->
-</main>
+        <table class="branch-table" role="presentation">
+          <tr>
+            <td>
+              <p class="df-mtitle fw600 gbd_red">Visa<sup>&reg;</sup> Credit Cards</p>
+              <p>
+
+                <strong>Customer Service:</strong> 800.423.7503<br />
+
+                <strong>Lost/Stolen:</strong> 800.325.3678<br />
+
+                <strong>Payment Address:</strong><br />
+                2040 Ingleside Avenue<br />
+                Macon, GA 31204<br />
+
+                For help with ezcardinfo call 866.604.0380
+              </p>
+            </td>
+            <td>
+              <p class="df-mtitle fw600 gbd_red">Visa<sup>&reg;</sup> Debit/ATM Cards</p>
+              <p><strong>Lost/Stolen:</strong> 800.472.3272</p>
+
+              <p class="df-mtitle fw600 gbd_red">Timely Teller</p>
+              <p><strong>24 Hour Phone Number:</strong> 478.744.0670</p>
+            </td>
+          </tr>
+
+        </table>
+
+
+        <div class="clearfix"></div>
+
+      </div><!--end nine columns-->
+
+      <div class="three columns sidebar">
+        <?php include('../includes/about-us-sidebar.php'); ?>
+      </div><!--end three columns sidebar-->
+
+    </div><!--end container-->
+  </main>
 
 
 
 
-<?php include ('../includes/footer.php');?>
+  <?php include('../includes/footer.php'); ?>
 
-<!--JAVASCRIPT INCLUDES-->
+  <!--JAVASCRIPT INCLUDES-->
 
 
 </body>
